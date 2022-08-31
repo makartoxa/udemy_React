d9d0d7008bba558397d7569e828f2dbd454ebab0
user2507 <makar.toxa@gmail.com>
2022-08-31 11:53:41 +0300

    add new file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import {Component} from "react";  // impotr React from "react"
 import './App.css';
+
+const Header = () => {
+    return <h2>Hello World!</h2>
+}
+
+// const Field = () => {
+//     const holder = 'Enter here'
+//     const styleField = {
+//         width: '350px'
+//     };
+//     return <input
+//         placeholder={holder}
+//         type="text"
+//         style={styleField}/>
+// };
+
+class Field extends Component {
+    render() {
+        const holder = 'Enter here'
+        const styleField = {
+            width: '350px'
+        }
+        return <input placeholder={holder}
+                      type="text"
+                      style={styleField}/>
+    }
+}
+
+const Btn = () => {
+    const text = "Log in";
+    // const res = () => {
+    //     return "Log in";
+    // }
+    // const p = <p>"Log in"</p>
+    const logged = false;
+    return <button>{logged ? "Enter" : text}</button>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Field/>
+        <Btn/>
     </div>
-  );
-}
+  )
+};
 
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Header} from './App';
+
+// const elem = <h2>Hello World!</h2>
+// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
+// const text = 'Hello World!';
+//
+// const elem = (
+//     <div>
+//         <h2 className={text}>Text: {text}!</h2>
+//         <input type="text"/>
+//         <label htmlFor=''></label>
+//         <button tabIndex={0}>Click</button>
+//     </div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App/>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
